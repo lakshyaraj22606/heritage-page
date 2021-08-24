b0cda85c390545c610df17452756629faf2051d8
lakshyaraj22606 <77723521+lakshyaraj22606@users.noreply.github.com>
2021-08-24 08:04:53 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Heritage from './pages/heritage';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return <Heritage />;
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+      let more = document.querySelectorAll('.more');
+      for (let i=0; i < more.length; i++){
+    more[i].addEventListener('click',function(){
+      more[i].parentNode.classList.toggle('active')
+    })
+      }
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
